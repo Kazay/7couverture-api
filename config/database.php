51e4b5ae46f567ccf546092c9d70875b5c89981d
Kevin Czaja <czajakevin@gmail.com>
2020-09-03 16:55:25 +0200

    db connec

--- config/database.php
+++ config/database.php
@@ -3,10 +3,22 @@
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
 return [
-'DB_CONNECTION' => 'pgsql',
-'DB_HOST' => $dbopts["host"],
-'DB_PORT' =>  $dbopts["port"],
-'DB_DATABASE' => ltrim($dbopts["path"],'/'),
-'DB_USERNAME' => $dbopts["user"],
-'DB_PASSWORD' => $dbopts["pass"],
+  'fetch' => PDO::FETCH_CLASS,
+  
+  'default' => env('DB_CONNECTION', 'pgsql'),
+
+  'connections' => [
+    'pgsql' => [
+      'driver'   => 'pgsql',
+      'host'     => $dbopts["host"],
+      'database' => ltrim($dbopts["path"],'/'),
+      'username' => $dbopts["user"],
+      'password' => $dbopts["pass"],
+      'charset'  => 'utf8',
+      'prefix'   => '',
+      'schema'   => 'public',
+    ],
+  ],
+
+  'migrations' => 'migrations',
 ];
